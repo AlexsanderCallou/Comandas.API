7cb8989b6709d01fea1a02c05121a791512e5d9b
AlexsanderCallou <alexsandercallou@gmail.com>
2025-04-15 15:48:13 -0300

    criado arquivos dtos.

---
+++ comandas.api/DTOs/MesaDTO.cs
@@ -0,0 +1,23 @@
+namespace Comandas.API.DTOs {
+
+    public class MesaGetDTO {
+        public  int Id { get; set; }
+        public int NumeroMesa { get; set; }
+        public int SituacaoMesa { get; set; }
+
+    }
+
+        public class MesaPostDTO {
+        public int NumeroMesa { get; set; }
+        public int SituacaoMesa { get; set; }
+
+    }
+
+        public class MesaPutDTO {
+        public  int Id { get; set; }
+        public int NumeroMesa { get; set; }
+        public int SituacaoMesa { get; set; }
+
+    }
+
+}
